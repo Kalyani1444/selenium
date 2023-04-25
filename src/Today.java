7d0dfcca4d776792e9afe1a27fafca7f9b24dca6
Kalyani1444 <rusthum768@gmail.com>
2023-04-25 21:50:48 +0530

    selenium not working

---
+++ src/Today.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.test.Saras;
+
+public class Today {
+
+	public void toddd() {
+		System.out.println("chrome ois not working");
+
+	}
+
+		public static void main(String[] args) {
+			
+			Today t = new Today();
+		
+		t.toddd();
+		}
+
+	}
+
+
---
+++ src/org/test/Saras.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class Saras {
+
+	
+	public void method() {
+		 System.out.println("duck");
+}
+		public static void main(String[] args) {
+			
+		Saras s = new Saras();
+		
+		s.method();
+		}
+
+	}
